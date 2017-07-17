0d898f3e365b264fc706b8010a413a20b730cade
Mike Potter <mike@mikenbob.com>
2017-07-17 12:31:08 -0600

    more options

--- main.go
+++ main.go
@@ -2,21 +2,63 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type handler struct{}
+
+var (
+	addr     = flag.String("addr", "http://127.0.0.1:8080", "listen address")
+	listener net.Listener
+	err      error
 )
 
 func main() {
-	port := flag.String("p", "8080", "listen port")
 	flag.Parse()
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, os.Kill, syscall.SIGTERM)
 
-	http.HandleFunc("/", handler)
-	log.Println("Listening on localhost:", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	h := &handler{}
+	s := http.Server{Handler: h}
+
+	url, err := url.Parse(*addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proto := "tcp"
+	laddr := url.Host
+
+	if url.Scheme == "unix" {
+		proto = "unix"
+		laddr = fmt.Sprintf("%s%s", url.Host, url.RequestURI())
+	}
+	fmt.Printf("Listening on %s\n", laddr)
+	listener, err := net.Listen(proto, laddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func(c chan os.Signal) {
+		// Wait for a SIGINT or SIGKILL:
+		sig := <-c
+		log.Printf("Caught signal %s: shutting down.", sig)
+		listener.Close()
+		os.Exit(0)
+	}(sigc)
+
+	log.Fatal(s.Serve(listener))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	dump, _ := httputil.DumpRequest(r, true)
 	w.Write(dump)
 }
